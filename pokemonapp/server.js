68bba7d7d48ad3c8345b23fd68f498be422d1c1d
Dylan De Brouwer <dylan.de.brouwer@outlook.com>
2025-05-27 13:55:55 +0000

    test server

--- pokemonapp/server.js
+++ pokemonapp/server.js
@@ -1,3 +1,29 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 const app = express();
+// Route naar index.html verander naar ejs na verandering
+/*
+app.get('/', (req, res) => {
+    res.render('index'); Renders index.ejs na aanmaak
+});
+*/
+// Serve static files
+app.use(express.static(__dirname));
 
+// Routes for different pages
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'contact.html'));
+});
+
+// Start server
+app.listen(3000, () => {
+    console.log('Server is running at http://localhost:3000');
+});
